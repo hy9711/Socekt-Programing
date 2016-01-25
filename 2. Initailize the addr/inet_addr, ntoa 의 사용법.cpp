0c6b4b78b50115339f686bccf6cc26a965c70e4b
hy9711 <tjwjdgur23@naver.com>
2016-01-25 18:40:31 +0900

    Bind
    Bind(), inet_ntoa(), inet_addr()

---
+++ 2. Initailize the addr/inet_addr, ntoa 의 사용법.cpp
@@ -0,0 +1,59 @@
+﻿#include<stdio.h>
+#include<iostream>
+#include<string.h>
+#include<winsock2.h>
+#pragma warning (disable:4996)
+using namespace std;
+
+void Error_handling(char *message);
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+
+	if (WSAStartup(MAKEWORD(2,2),&wsaData)!=0)
+		Error_handling("WSA() error!");
+
+	//inet_addr() 사용하기.
+	// inet_addr 함수의 정의 : in_addr_t inet_addr(const char *string) => 반환하는 것은 sin_addr.s_addr형을 반환(즉 unsigned long)하고 문자형으로 정의된 주소(우리가 보는주소)를 인수로 받음.
+	{
+		char *serv = "188.223.11.20"; // inet_addr의 인수때문에 문자형으로 주소 초기화.
+		unsigned long conv_addr = inet_addr(serv); //inet_addr이 반환하는 형은 sin_addr.s_addr or unsigned long 형이다.
+
+		if (conv_addr == INADDR_NONE)
+			Error_handling("inet_addr() error!");
+		else
+			printf("컴퓨터식으로 변환된 주소 : %#lx\n\n", conv_addr);
+	}
+
+	//inet_ntoa() 사용하기.
+	//inet_ntoa 함수의 정의 : char *inet_ntoa(in_addr adr) => 반환하는 것은 문자형(즉 우리가 보는 주소)이고, sin_addr or 컴퓨터 주소를 인수로 받는다.
+	{
+		SOCKADDR_IN addr1, addr2; //inet_ntoa의 인수는 sin_addr 임으로 반드시 sockaddr_in 구조체 형인 변수를 선언 해야한다.
+		char *serv1 = "127.220.120.201";
+		char *str;//inet_ntoa의 반환형은 char *.
+		char *memo1, *memo2;
+		
+		addr1.sin_addr.s_addr = htonl(0x2030405);
+		addr2.sin_addr.s_addr = inet_addr(serv1); //inet_ntoa의 인수는 sin_addr임으로 반드시 컴퓨터형 주소로 변환해야한다.
+
+		str = inet_ntoa(addr1.sin_addr);
+		memo1 = new char[strlen(str)+1];
+		strcpy(memo1, str);
+
+		str = inet_ntoa(addr2.sin_addr);
+		memo2 = new char[strlen(str)+1];
+		strcpy(memo2, str);
+
+		printf("우리식으로 알 수 있는 주소 addr1 : %s", memo1);
+		cout << endl;
+		printf("우리식으로 알 수 있는 주소 addr2 : %s", memo2);
+		cout << endl;
+
+		delete[]memo1;
+		delete[]memo2;
+	}
+}
+void Error_handling(char *message)
+{
+	cout << message << endl;
+}
---
+++ 2. Initailize the addr/주소정보 초기화와 bind 함수의 사용(실행안됨).c
@@ -0,0 +1,45 @@
+﻿#include<stdio.h>
+#include<string.h>
+#include<WinSock2.h>
+
+void a(char *message)
+{
+	printf("%s", message);
+}
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET hServ_Sock;
+	SOCKADDR_IN serv_addr;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) //winsock 라이브러리 초기화
+		a("WSAStartup() error!");
+
+	hServ_Sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP); //소켓 생성.
+	if (hServ_Sock == INVALID_SOCKET)
+		a("socket() error");
+
+	//주소 초기화
+	memset(&serv_addr, 0, sizeof(serv_addr));//sockaddr_in 구조체의 크기와 sockaddr의 구조체 크기를 맞추기 위하여 memset함수 사용.
+	serv_addr.sin_family = AF_INET;//INETv4 주소체계 사용.
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); //=>serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	serv_addr.sin_port = htons(atoi(argv[2]));
+	//INADDR_ANY 는 자신의 호스트 IP주소를 자동으로 초기화 해주는 키워드이다.
+	/*
+	보통 ip주소나 port번호는 char 형으로 선언되어지는데 이를 정수형으로 바꿔주기 위해 ip주소는 inet_addr 함수를 사용하고
+	port 번호는 atoi함수를 사용한다. 두 함수 모두 인수는 char *형으로 선언된 우리가 알아볼 수 있는 주소이다.
+	*/
+
+	/*
+	bind 함수의 설명 :
+	int bind(SOCKET s, struct SOCKADDR *addr, int addrlen);
+	인수를 소켓핸들, SOCKADDR 구조체의 주소(즉, 여기에는 주소체계, PORT번호, IP주소가 모두 SOCKADDR_IN으로 저장되어서 보내짐->반드시 (SOCKADDR *)로 형변환), SOCKADDR_IN으로 선언된 변수의 길이
+	가 들어간다.
+	*/
+	if (bind(hServ_Sock, (SOCKADDR *)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR)
+		a("bind() error!");
+
+	WSACleanup();
+	closesocket(hServ_Sock);
+}
