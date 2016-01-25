c867b8e0f01cbac3c31db70094b0f8a1300ca4b6
hy9711 <tjwjdgur23@naver.com>
2016-01-25 19:10:17 +0900

    Echo Server & Echo Client
    Echo Server & Echo Client

---
+++ 3. echo/echo_client_win.c
@@ -0,0 +1,66 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<WinSock2.h>
+#define buf 1024
+#pragma warning(disable : 4996)
+void error(char *message);
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET sock;
+	SOCKADDR_IN hServ_addr;
+	char message[buf];
+	int strLen;
+
+	if (argc != 3)
+	{
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		error("WSAStartup() error!");
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+		error("socket() error");
+
+	memset(&hServ_addr, 0, sizeof(hServ_addr));
+	hServ_addr.sin_family = AF_INET;
+	hServ_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	hServ_addr.sin_port = htons(atoi(argv[2]));
+
+	if (connect(sock, (SOCKADDR *)&hServ_addr, sizeof(hServ_addr)) == SOCKET_ERROR)
+		error("connect() error");
+	else
+		puts("Connected.......");
+
+	/*
+	echo 클라이언트는 서버로 data를 전송하고 전송한 data를 서버로 부터 다시 재전송 받는다.
+	*/
+	while (1)
+	{
+		fputs("Input message(Q to quit) : ", stdout);
+		fgets(message, buf, stdin); // message 입력.
+		
+		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+			break;
+
+		send(sock, message, strlen(message), 0); //message를 서버로 발송.
+		strLen = recv(sock, message, buf, 0); //보낸 message를 서버로 부터 다시 받음.
+		message[strLen] = 0;
+		printf("Message from server : %s\n", message);
+	}
+
+	closesocket(sock);
+	WSACleanup();
+	return 0;
+}
+
+void error(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
---
+++ 3. echo/echo_server_win.c
@@ -0,0 +1,67 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<WinSock2.h>
+#define buf 1024
+void error(char *message);
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET hServ, hClnt;
+	SOCKADDR_IN hserv_addr, hClnt_addr;
+	char message[buf];
+	int szClnt, i, strLen;
+
+	if (argc != 2)
+	{
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		error("WSAStartup() error");
+
+	hServ = socket(PF_INET, SOCK_STREAM, 0);
+	if (hServ == INVALID_SOCKET)
+		error("socket() error");
+
+	memset(&hserv_addr, 0, sizeof(hserv_addr));
+	hserv_addr.sin_family = AF_INET;
+	hserv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	hserv_addr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(hServ, (SOCKADDR *)&hserv_addr, sizeof(hserv_addr)) == SOCKET_ERROR)
+		error("bind() error");
+
+	if (listen(hServ, 5) == SOCKET_ERROR)
+		error("listen() error");
+
+	/*
+	echo 서버란 ? 클라이언트가 보낸 data를 수신하고 수신한 data를 다시 클라이언트에게 재 전송하는 서버를 말한다.
+	*/
+	szClnt = sizeof(hClnt_addr);
+	for (i = 0; i < 5; i++) //대기 큐 목록. 
+	{
+		// 연결 승인.
+		hClnt = accept(hServ, (SOCKADDR *)&hClnt_addr, &szClnt);
+		if (hClnt == INVALID_SOCKET)
+			error("accept() error");
+		else
+			printf("Connected client %d \n", i + 1);
+
+		while ((strLen = recv(hClnt, message, buf, 0)) != 0) // 클라이언트로 부터 data를 수신받고
+			send(hClnt, message, strLen, 0); // 수신 받은 data를 다시 클라이언트에게 전송한다.
+
+		closesocket(hClnt);
+	}
+	closesocket(hServ);
+	WSACleanup();
+	return 0;
+}
+
+void error(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
