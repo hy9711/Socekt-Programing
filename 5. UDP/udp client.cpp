d2c19d6105b5067dd24b81cdb3fdd1bca5d56569
hy9711 <tjwjdgur23@naver.com>
2016-02-25 03:22:25 +0900

    UDP
    This sorce is example about very simple udp.

---
+++ 5. UDP/udp client.cpp
@@ -0,0 +1,63 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<WinSock2.h>
+#include<iostream>
+using namespace std;
+#define BUF_SIZE 30
+#pragma warning(disable : 4996)
+void ErrorHandling(char *message);
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET sock;
+	char message[BUF_SIZE];
+	int strLen;
+	int adrSz;
+
+	SOCKADDR_IN servAdr, fromAdr;
+
+	if (argc != 3)
+	{
+		cout << "Usage : " << argv[0] << " <IP> <Port>" << endl;
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		ErrorHandling("WSAStartup() error!");
+	}
+
+	sock = socket(PF_INET, SOCK_DGRAM, 0);
+
+	if (sock == INVALID_SOCKET)
+		ErrorHandling("sock() error");
+
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.s_addr = inet_addr(argv[1]);
+	servAdr.sin_port = htons(atoi(argv[2]));
+	
+	while (1)
+	{
+		fputs("Insert message(q to quit) : ", stdout);
+		fgets(message, sizeof(message), stdin);
+		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+			break;
+		sendto(sock, message, strlen(message), 0,(SOCKADDR *)&servAdr, sizeof(servAdr));
+		adrSz = sizeof(fromAdr);
+		strLen = recvfrom(sock, message, BUF_SIZE, 0, (SOCKADDR *)&fromAdr, &adrSz);
+		message[strLen] = 0;
+		cout << "Message from server : " << message << endl;
+	}
+	closesocket(sock);
+	return 0;
+}
+
+void ErrorHandling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
---
+++ 5. UDP/udp connected client.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<WinSock2.h>
+using namespace std;
+#define BUF_SIZE 30
+#pragma warning(disable : 4996)
+void ErrorHandling(char *message);
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET sock;
+	SOCKADDR_IN toAdr;
+
+	if (argc != 3)
+	{
+		cout << "Usage : " << argv[0] << " <IP> <PORT>" << endl;
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("startup() Error!");
+
+	sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (sock == INVALID_SOCKET)
+		ErrorHandling("sock() Error!");
+
+	memset(&toAdr, 0, sizeof(toAdr));
+	toAdr.sin_family = AF_INET;
+	toAdr.sin_addr.s_addr = inet_addr(argv[1]);
+	toAdr.sin_port = htons(atoi(argv[2]));
+
+	connect(sock, (SOCKADDR *)&toAdr, sizeof(toAdr));
+
+	char message[BUF_SIZE];
+	while (1)
+	{
+		fputs("Insert message(q to quit) : ", stdout);
+		fgets(message, sizeof(message), stdin);
+		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+			break;
+
+		send(sock, message, strlen(message), 0);
+		int strLen = recv(sock, message, sizeof(message) - 1, 0);
+		message[strLen] = '\0';
+		cout << "Message From Server : " << message;
+	}
+
+	closesocket(sock);
+	WSACleanup();
+	return 0;
+}
+
+void ErrorHandling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
