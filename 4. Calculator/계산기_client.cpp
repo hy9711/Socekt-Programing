2054d562194c2e1d13eb4cc764a46097c97acf9e
hy9711 <tjwjdgur23@naver.com>
2016-02-25 03:18:23 +0900

    Calculator
    User input the number and operator in client. And the user get answer
    about expression in server.

---
+++ 4. Calculator/계산기_client.cpp
@@ -0,0 +1,78 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<WinSock2.h>
+#pragma warning (disable : 4996)
+#define INT 4
+void error(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+int main(int argv, char *argc[])
+{
+	WSADATA wsaData;
+	SOCKET hclnt;
+	SOCKADDR_IN serv_addr;
+
+	if (argv != 3)
+	{
+		printf("<Usage> %s\n", argc[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		error("초기화 error");
+
+	hclnt = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (hclnt == INVALID_SOCKET)
+		error("socket error");
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argc[1]);
+	serv_addr.sin_port = htons(atoi(argc[2]));
+
+	if (connect(hclnt, (SOCKADDR*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR)
+		error("conect error");
+	else
+		puts("Connected...............");
+
+	int count;
+	char *opmsg;
+	fputs("Operand count : ", stdout);
+	scanf("%d", &count);
+	opmsg = new char[count * INT + 3];
+	opmsg[0] = (char)count;
+
+	for (int i = 0; i < count; i++)
+	{
+		printf("Number %d : ", i + 1);
+		scanf("%d", (int *)&opmsg[INT*i + 1]);
+	}
+	char result[4];
+	fgetc(stdin);
+	fputs("Operator : ", stdout);
+	scanf("%c", &opmsg[INT*count + 1]);
+	send(hclnt, opmsg, INT*count + 2, 0);
+
+	int recvlen = 0;
+	while (recvlen < 4)
+	{
+		static int z = 0;
+		if ((z = (recv(hclnt, &result[recvlen], INT, 0))) == -1)
+			error("recv error");
+		else
+			recvlen += z;
+	}
+
+	int *resulted = (int *)result;
+
+	printf("Operation result : %d\n", *resulted);
+	closesocket(hclnt);
+	WSACleanup();
+	delete[]opmsg;
+	return 0;
+}
---
+++ 4. Calculator/계산기_server.cpp
@@ -0,0 +1,109 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<WinSock2.h>
+#include<iostream>
+#pragma warning (disable : 4996)
+#define INT 4
+void error(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+int cal(int count, int* number, char op);
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET serv, clnt;
+	SOCKADDR_IN serv_addr, clnt_addr;
+
+	if (argc != 2)
+	{
+		printf("<Usage> %s\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		error("초기화 error");
+
+	serv = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (serv == INVALID_SOCKET)
+		error("socket error");
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(serv, (SOCKADDR *)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR)
+		error("bind() error");
+
+	if (listen(serv, 5) == SOCKET_ERROR)
+		error("listen() error");
+
+	int szClnt = sizeof(clnt_addr);
+	char count;
+
+	for (int i = 0; i < 5; i++)
+	{
+		clnt = accept(serv, (SOCKADDR *)&clnt_addr, &szClnt);
+		if (clnt == INVALID_SOCKET)
+			error("accept error");
+		else
+			printf("connected %d\n", i + 1);
+
+		recv(clnt, &count, 1, 0);
+		int int_count = (int)count;
+		int recv_len = 0;
+		char *number = new char[int_count*INT + 3];
+
+		while ((INT*int_count + 1) > recv_len)
+		{
+			static int z = 0;
+			if ((z = (recv(clnt, &number[recv_len], INT, 0))) == -1)
+				error("recv() error");
+			else
+				recv_len += z;
+		}
+		int result;
+		result = cal(int_count, (int *)number, number[recv_len - 1]);
+
+		send(clnt, (char *)&result, INT, 0);
+		delete[]number;
+		closesocket(clnt);
+	}
+	closesocket(serv);
+	WSACleanup();
+	return 0;
+}
+
+int cal(int count, int* number, char op)
+{
+	int result = number[0];
+
+	switch (op)
+	{
+	case '+':
+	{
+		for (int i = 1; i < count; i++)
+			result += number[i];
+		break;
+	}
+	case '-':
+	{
+		for (int i = 1; i < count; i++)
+			result -= number[i];
+		break;
+	}
+	case '*':
+	{
+		for (int i = 1; i < count; i++)
+			result *= number[i];
+		break;
+	}
+	}
+
+	return result;
+}
